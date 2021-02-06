dabf4c148c5ab6f0c521e76d58417914b56aca3f
suod-NithishKarthik <nithishkarthik01@gmail.com>
2021-02-06 16:30:36 +0530

    Established Db connection

--- stu-act-manager/backend/app.js
+++ stu-act-manager/backend/app.js
@@ -1,41 +1,38 @@
-var createError = require('http-errors');
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Route imports
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
+var mongoose = require('mongoose');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//Connecting to DATABASE
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+  useCreateIndex: true
+})
+.then(() => console.log("DB connection succesful!"))
+.catch(err => console.log(err))
 
+
+//Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routes 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
